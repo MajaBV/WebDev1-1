8dea4ec2d08de76ea31c005b126738e0780ea5ba
Maja Vukadin <maja.barisic5@gmail.com>
2019-12-16 23:03:28 +0100

    initial commit - task 6

---
+++ zadatak_6.py
@@ -0,0 +1,8 @@
+dog_years = float(input("How old the dog is: "))
+if 0 < dog_years <= 2:
+        years = dog_years * 10.5
+        print(years)
+elif dog_years > 2:
+        dog_years2 = dog_years - 2
+        years2 = 21 + dog_years2 * 4
+        print(years2)
